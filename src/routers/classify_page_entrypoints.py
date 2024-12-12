13719cb27403900bc2abdbc35488a6f596b13992
Oz Shaked <oz.rnds@gmail.com>
2024-12-12 23:46:34 +0000

    feat(ClassifyRouter): Update faces ron_in_image field when the image is updated

--- src/routers/classify_page_entrypoints.py
+++ src/routers/classify_page_entrypoints.py
@@ -1,15 +1,19 @@
+from typing import List
+
 from fastapi import FastAPI, exceptions
 
+from src.services.faces_db_service import FaceDBService
 from src.services.groups_db import load_groups_from_file, sort_and_save_groups
 from src.utils.model_pydantic import (
+    Face,
     UpdateClassificationRequest,
     UpdateRonInImageRequest,
 )
 
 
 class ClassifyRouter:
-    def __init__(self):
-        pass
+    def __init__(self, face_db_service: FaceDBService):
+        self._face_db_service = face_db_service
 
     def create_entry_points(self, app: FastAPI):
         # Endpoint to update the image classification
@@ -51,6 +55,7 @@ class ClassifyRouter:
                     for image in group["list_of_images"]:
                         if image["name"] == request.image_name:
                             image["ron_in_image"] = request.ron_in_image
+                            image_full_path = image["full_client_path"]
                             break
                     break
 
@@ -58,8 +63,18 @@ class ClassifyRouter:
                 raise exceptions.HTTPException(
                     status_code=404, detail="Group not found"
                 )
-
+            self.update_images_face(
+                image_full_path=image_full_path, ron_in_image=request.ron_in_image
+            )
             # Save updated metadata
             sort_and_save_groups(grouped_metadata)
-
             return {"message": "Ron in image flag updated successfully"}
+
+    def update_images_face(self, image_full_path: str, ron_in_image: bool):
+        faces_to_update: List[Face] = self._face_db_service.get_faces(
+            {"image_full_path": image_full_path}
+        )
+
+        for face in faces_to_update:
+            face.ron_in_image = ron_in_image
+            self._face_db_service.add_face(face)
