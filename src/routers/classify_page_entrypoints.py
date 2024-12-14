ad78c3c200a1c0e777678c084bb7c1976f8cd883
Oz Shaked <oz.rnds@gmail.com>
2024-12-14 21:53:11 +0000

    feat(ClassifyRouterV2): Add new classify entry points that support the new db type

--- src/routers/classify_page_entrypoints.py
+++ src/routers/classify_page_entrypoints.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI, exceptions, status
 
 from src.services.faces_db_service import FaceDBService
 from src.services.groups_db import load_groups_from_pickle_file, sort_and_save_groups
+from src.services.groups_db_service import GroupDBService
+from src.services.images_db_service import ImageDBService
 from src.utils.model_pydantic import (
     Face,
     UpdateClassificationRequest,
@@ -11,7 +13,7 @@ from src.utils.model_pydantic import (
 )
 
 
-class ClassifyRouter:
+class ClassifyRouterV1:
     def __init__(self, face_db_service: FaceDBService):
         self._face_db_service = face_db_service
 
@@ -84,3 +86,67 @@ class ClassifyRouter:
         for face in faces_to_update:
             face.ron_in_image = ron_in_image
             self._face_db_service.add_face(face)
+
+
+class ClassifyRouterV2(ClassifyRouterV1):
+    def __init__(
+        self, image_db_service: ImageDBService, group_db_service: GroupDBService
+    ):
+        self._image_db_service = image_db_service
+        self._group_db_service = group_db_service
+
+    def create_entry_points(self, app: FastAPI):
+        # Endpoint to update the image classification
+        @app.post("/update_image_classification", tags=["Images"])
+        async def update_image_classification(request: UpdateClassificationRequest):
+            group = self._group_db_service.get_group(request.group_name)
+
+            if not group:
+                raise exceptions.HTTPException(
+                    status_code=404, detail="Group not found"
+                )
+
+            image_found = False
+            for image_path in group.list_of_images:
+                image = self._image_db_service.get_images({"name": request.image_name})
+                if image:
+                    image_found = True
+                    image[0].classification = request.classification
+                    self._image_db_service.add_image(image[0], flush=True)
+                    break
+
+            if not image_found:
+                raise exceptions.HTTPException(
+                    status_code=404, detail="Image not found in group"
+                )
+
+            return {"message": "Image classification updated successfully"}
+
+        # Endpoint to update the 'Ron in the image' flag
+        @app.post("/update_ron_in_image", tags=["Images"])
+        async def update_ron_in_image(request: UpdateRonInImageRequest):
+            group = self._group_db_service.get_group(request.group_name)
+
+            if not group:
+                raise exceptions.HTTPException(
+                    status_code=404, detail="Group not found"
+                )
+
+            image_found = False
+            for image_path in group.list_of_images:
+                image = self._image_db_service.get_images({"name": request.image_name})
+                if image:
+                    image_found = True
+                    image[0].ron_in_image = request.ron_in_image
+                    self._image_db_service.add_image(image[0], flush=True)
+                    self.update_images_face(
+                        image[0].full_client_path, request.ron_in_image
+                    )
+                    break
+
+            if not image_found:
+                raise exceptions.HTTPException(
+                    status_code=404, detail="Image not found in group"
+                )
+
+            return {"message": "Ron in image flag updated successfully"}
